3abc3101fac4563f9f91ae9bb5f936ac864c8c34
Vimal Sonara <vvsonara95@gmail.com>
2023-06-07 09:31:02 +0530

    Set Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
+import { Routes, Route } from 'react-router-dom';
+import useAuthStore from './store/useAuthStore';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import Bookings from './pages/Bookings';
+import AuthRoutes from './routes/AuthRoutes';
+import AdminRoutes from './routes/AdminRoutes';
 import './App.css';
 
 function App() {
+  const isAuthenticated = useAuthStore((state) => state.isAuthenticated);
+  console.log(isAuthenticated);
+  const authRoutes = (
+    <Routes>
+      <Route exact path="" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
+
+  const adminRoutes = (
+    <Routes>
+      <Route path="" index element={<Dashboard />} />
+      <Route path="/bookings" index element={<Bookings />} />
+    </Routes>
+  );
+
   return (
     <>
-      <div></div>
+      {!isAuthenticated ? (
+        <AuthRoutes>{authRoutes}</AuthRoutes>
+      ) : (
+        <AdminRoutes>{adminRoutes}</AdminRoutes>
+      )}
     </>
   );
 }
