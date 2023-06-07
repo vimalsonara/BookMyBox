571290945fd9f9f86692244dcb0fb936bd91ca10
Vimal Sonara <vvsonara95@gmail.com>
2023-06-07 09:35:30 +0530

    Add Admin & Auth route page

---
+++ src/routes/AdminRoutes.jsx
@@ -0,0 +1,9 @@
+function AdminRoutes({ children }) {
+  return (
+    <div className="h-screen bg-gradient-to-br from-blue-500 to-purple-500">
+      {children}
+    </div>
+  );
+}
+
+export default AdminRoutes;
---
+++ src/routes/AuthRoutes.jsx
@@ -0,0 +1,9 @@
+function AuthRoutes({ children }) {
+  return (
+    <div className="h-screen bg-gradient-to-br from-blue-500 to-purple-500">
+      {children}
+    </div>
+  );
+}
+
+export default AuthRoutes;
