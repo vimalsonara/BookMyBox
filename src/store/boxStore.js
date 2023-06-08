0880c3208e6e0c2f51a64804763f89ab372d585c
Vimal Sonara <vvsonara95@gmail.com>
2023-06-08 09:34:49 +0530

    Localstorage to store & clear box data

--- src/store/boxStore.js
+++ src/store/boxStore.js
@@ -1,8 +1,17 @@
 import { create } from 'zustand';
 
 const useBoxesStore = create((set) => ({
-  boxes: [],
-  setBoxes: (newBoxes) => set({ boxes: newBoxes }),
+  boxes: localStorage.getItem('boxes')
+    ? JSON.parse(localStorage.getItem('boxes'))
+    : [],
+  setBoxes: (newBoxes) => {
+    set({ boxes: newBoxes });
+    localStorage.setItem('boxes', JSON.stringify(newBoxes));
+  },
+  clearBoxes: () => {
+    set({ boxes: [] });
+    localStorage.removeItem('boxes');
+  },
 }));
 
 export default useBoxesStore;
