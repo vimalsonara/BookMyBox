a041e7d09b1dbe52dd6cb253bc242b135cf7d490
Vimal Sonara <vvsonara95@gmail.com>
2023-06-08 22:21:19 +0530

    Add delete box button & functionality

--- src/components/box/Box.jsx
+++ src/components/box/Box.jsx
@@ -1,6 +1,7 @@
 import { PropTypes } from 'prop-types';
 import { useState } from 'react';
 import NewBooking from '../booking/NewBooking';
+import { db } from '../../appwrite/appwriteConfig';
 
 function Box(props) {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,15 +13,41 @@ function Box(props) {
     setIsOpen(false);
   }
 
+  async function deleteBox() {
+    const confirmDelete = window.confirm(
+      'Are you sure you want to delete this box?'
+    );
+    if (confirmDelete) {
+      try {
+        await db.deleteDocument(
+          import.meta.env.VITE_APPWRITE_DATABASE_ID,
+          import.meta.env.VITE_APPWRITE_BOX_COLLECTION_ID,
+          boxId
+        );
+
+        // Refresh the page
+        window.location.reload();
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  }
+
   return (
     <>
       <div className="flex h-40 w-40  flex-col items-center justify-center rounded-md  bg-gradient-to-r from-green-300   to-green-500 shadow shadow-black hover:bg-gradient-to-l ">
-        <h3>{props.boxName}</h3>
+        <h3 className="text-lg font-bold">{props.boxName}</h3>
         <button
           className="mt-3 h-10 w-20 rounded-md border border-white p-2 font-bold text-white hover:border-black hover:font-bold  hover:text-black hover:shadow-md hover:shadow-black"
           onClick={() => setIsOpen(true)}
         >
           Book
+        </button>
+        <button
+          className="mt-3 outline-yellow-500 h-10 w-20 rounded-md border border-white p-2 font-bold text-black bg-yellow-500   hover:bg-yellow-600 hover:border-black hover:font-bold  hover:text-white hover:shadow-md hover:shadow-black"
+          onClick={deleteBox}
+        >
+          Delete
         </button>
       </div>
 
